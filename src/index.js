9fedf69f1b3ad33876ce8e5f6e4d5e0dfd914943
Maxim Zorin <maxim.zorin@dev.zodiac.tv>
2021-12-28 18:15:26 +0300

    feat: solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const openBrackets = Object.fromEntries(bracketsConfig);
+  const closeBrackets = bracketsConfig.map(x => x[1]);
+  for (const letter of str) {
+    if (closeBrackets.includes(letter)) {
+      if (letter === openBrackets[stack[0]])
+        stack.shift();
+      else if (openBrackets[letter])
+        stack.unshift(letter);
+      else
+        return false;
+    }
+    else if (openBrackets[letter])
+      stack.unshift(letter);
+  }
+  return stack.length === 0;
 }
